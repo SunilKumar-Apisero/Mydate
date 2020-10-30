122ee3589a10fe092c752be10247a3a1c17335b3
SunilKumar-Apisero <73459853+SunilKumar-Apisero@users.noreply.github.com>
2020-10-30 15:20:15 +0530

    Update MyDate.java

--- MyDate.java
+++ MyDate.java
@@ -4,26 +4,12 @@
 import java.util.*;
 class MyDate
 {
- private int day,month,year;
-MyDate()
+    public static void main(String args[])
 {
-day=20; month=10;year=2020;
-}
-MyDate(int d,int m,int y)
-{
-day=d; month=m;year=y;
-}
-void display()
-{
-System.out.println(day+"/"+month+"/"+year);
-}
+    int  day=20; month=10;year=2020;
+     System.out.println(day + month + year);
 
-public static void main(String args[])
-{
 
-MyDate d=new MyDate();
-MyDate d1=new MyDate(19,10,2020);
-d.display();
-d1.display();
-}
+    
+    }
 }
