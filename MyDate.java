e4a2ef44e626e04f6f28413f90b2639a6c5ef00b
SunilKumar-Apisero <73459853+SunilKumar-Apisero@users.noreply.github.com>
2020-10-30 15:33:19 +0530

    Update MyDate.java

--- MyDate.java
+++ MyDate.java
@@ -6,7 +6,7 @@ class MyDate
 {
     public static void main(String args[])
 {
-    int  day=20; month=10;year=2020;
+    int  day=20, month=10, year=2020;
      System.out.println(day + month + year);
 
 
